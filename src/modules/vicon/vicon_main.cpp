aaeba836b389334aa56d891d867365cc03ec4581
gfink <gfink@ualberta.ca>
2017-02-16 15:31:14 -0700

    Add the ability to calculate frequency of vicon data

--- src/modules/vicon/vicon_main.cpp
+++ src/modules/vicon/vicon_main.cpp
@@ -53,7 +53,7 @@
 #include <functional>
 #include <drivers/drv_hrt.h>
 #include <arch/board/board.h>
-
+#include <poll.h>
 #include <uORB/topics/vicon.h>
 
 
@@ -64,33 +64,68 @@
  */
 extern "C" __EXPORT int vicon_main(int argc, char *argv[]);
 
+int print_vicon_status();
+int print_vicon_hz();
 
 int vicon_main(int argc, char *argv[])
 {
-	int sub=-1;
-	struct vicon_s data;
-	memset(&data,0,sizeof(data));
-
 	if (argc < 2) {
-		warnx("usage: vicon {status}");
+		warnx("usage: vicon {status|hz}");
 		return 1;
 	}
-
-	if (!strcmp(argv[1], "status")) {
-		sub = orb_subscribe(ORB_ID(vicon));
-		if (sub>0) {
-			orb_copy(ORB_ID(vicon), sub, &data);
-			PX4_INFO("Vicon:");
-			PX4_INFO("pos: (%2.3f,%2.3f,%2.3f)",(double)data.p[0],(double)data.p[1],(double)data.p[2]);
-			PX4_INFO("vel: (%2.3f,%2.3f,%2.3f)",(double)data.v[0],(double)data.v[2],(double)data.v[2]);
-			PX4_INFO("q: (%2.3f,%2.3f,%2.3f,%2.3f)",(double)data.q[0],(double)data.q[1],(double)data.q[2],(double)data.q[3]);
-			PX4_INFO("received @ %" PRIu64 " / %" PRIu64 "\n      sent @ %" PRIu64, data.t_local, hrt_absolute_time(), data.t_remote);
-			sub = orb_unsubscribe(sub);
-		} else {
-			PX4_INFO("Could not subscribe to Vicon topic");
-		}
-		return 0;
-	}
+	if (!strcmp(argv[1], "status")) return print_vicon_status();
+	if (!strcmp(argv[1], "hz")) return print_vicon_hz();
+	
 	warnx("unrecognized command");
 	return 1;
+}
+
+int print_vicon_status() {
+	int sub = -1;
+	sub = orb_subscribe(ORB_ID(vicon));
+	if (sub>0) {
+		struct vicon_s data;
+		memset(&data,0,sizeof(data));
+		orb_copy(ORB_ID(vicon), sub, &data);
+		PX4_INFO("Vicon:");
+		PX4_INFO("pos: (%2.3f,%2.3f,%2.3f)",(double)data.p[0],(double)data.p[1],(double)data.p[2]);
+		PX4_INFO("vel: (%2.3f,%2.3f,%2.3f)",(double)data.v[0],(double)data.v[2],(double)data.v[2]);
+		PX4_INFO("q: (%2.3f,%2.3f,%2.3f,%2.3f)",(double)data.q[0],(double)data.q[1],(double)data.q[2],(double)data.q[3]);
+		PX4_INFO("received @ %" PRIu64 " / %" PRIu64 "\n      sent @ %" PRIu64, data.t_local, hrt_absolute_time(), data.t_remote);
+		sub = orb_unsubscribe(sub);
+	} else {
+		PX4_INFO("Could not subscribe to Vicon topic");
+		return 1;
+	}
+	return 0;
+}
+
+int print_vicon_hz() {
+	int sub = -1;
+	sub = orb_subscribe(ORB_ID(vicon));
+	if (sub>0) {
+		px4_pollfd_struct_t fds;
+		fds.fd = sub;
+		fds.events = POLLIN;
+		uint64_t t0 = hrt_absolute_time();
+		uint64_t t = 0;
+		uint64_t count = 0;
+		while (t<5000000 && count < 250) { //5 s or 250 msgs
+			px4_poll(&fds, 1, 1000);	//1 s
+			t=hrt_absolute_time()-t0;
+			if (fds.revents & POLLIN) {
+				orb_copy(ORB_ID(vicon), sub, NULL);
+				count++;
+			}
+		}
+		PX4_INFO("Vicon:");
+		PX4_INFO("count = %" PRIu64, count); 
+		PX4_INFO("time = %" PRIu64 " ms", t/1000); 
+		PX4_INFO( "%3.1f Hz", double(count)/((double)t/(double)1000000));
+	} else {
+		PX4_INFO("Could not subscribe to Vicon topic");
+		return 1;
+	}
+	return 0;		
+
 }
